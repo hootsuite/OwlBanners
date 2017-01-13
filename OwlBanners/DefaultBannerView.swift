c5a4172a97a63042a1a1ad9fe54ae205e955826f
Johnny Ho <johnny.ho@hootsuite.com>
2017-01-13 09:09:23 -0800

    IOS-6938 - changed it to notice instead

--- OwlBanners/DefaultBannerView.swift
+++ OwlBanners/DefaultBannerView.swift
@@ -4,10 +4,10 @@ import UIKit
 
 /// Defines banner default styles.
 public enum DefaultBannerStyle: BannerStyle {
+    case notice
     case success
     case warning
     case error
-    case persistentNotice
     case persistentError
     case info
 
@@ -17,6 +17,9 @@ public enum DefaultBannerStyle: BannerStyle {
         var requireUserDismissal = false
 
         switch self {
+        case .notice:
+            view.backgroundColor = UIColor(red: 0.424, green: 0.647, blue: 0.117, alpha: 1)
+            view.set(icon: nil, accessibilityLabel: "Notice")
         case .success:
             view.backgroundColor = UIColor(red: 0.424, green: 0.647, blue: 0.117, alpha: 1)
             let icon = UIImage(named: "ic_banner_complete", in: bundle, compatibleWith: nil)
@@ -29,11 +32,6 @@ public enum DefaultBannerStyle: BannerStyle {
             view.backgroundColor = UIColor(red: 0.816, green: 0.286, blue: 0.286, alpha: 1)
             let icon = UIImage(named: "ic_banner_error", in: bundle, compatibleWith: nil)
             view.set(icon: icon, accessibilityLabel: "Error")
-        case .persistentNotice:
-            view.backgroundColor = UIColor(red: 0.424, green: 0.647, blue: 0.117, alpha: 1)
-            let icon = UIImage(named: "ic_banner_complete", in: bundle, compatibleWith: nil)
-            view.set(icon: icon, accessibilityLabel: "Success")
-            requireUserDismissal = true
         case .persistentError:
             view.backgroundColor = UIColor(red: 0.816, green: 0.286, blue: 0.286, alpha: 1)
             let icon = UIImage(named: "ic_banner_error", in: bundle, compatibleWith: nil)
