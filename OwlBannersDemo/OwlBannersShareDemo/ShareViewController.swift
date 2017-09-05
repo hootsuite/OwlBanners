4322cc1d8722d5e5fc1549594c671340cd7a803e
Taylor Ledingham <taylor.ledingham@hootsuite.com>
2017-09-05 11:45:55 -0700

    CMOB-220: update swift lint and fix warnings (#19)

--- OwlBannersDemo/OwlBannersShareDemo/ShareViewController.swift
+++ OwlBannersDemo/OwlBannersShareDemo/ShareViewController.swift
@@ -1,5 +1,5 @@
 // Copyright 2017 HootSuite Media Inc.
-//
+
 // This file is part of OwlBanners. The full OwlBanners copyright notice,
 // including terms governing use, modification, and redistribution, is
 // contained in the file LICENSE.md at the root of the source code distribution
