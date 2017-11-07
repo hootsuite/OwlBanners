6eac973117ca0abcd63c8619a5384ddcb1e8816a
Alejandro Isaza <alejandro.isaza@hootsuite.com>
2017-11-07 09:56:25 -0800

    Fix comment block

--- Sources/OwlBanners/OwlBanners.h
+++ Sources/OwlBanners/OwlBanners.h
@@ -1,10 +1,9 @@
-//
-//  OwlBanners.h
-//  OwlBanners
-//
-//  Created by Brett Stover on 10/8/15.
-//  Copyright © 2015 Hootsuite. All rights reserved.
-//
+// Copyright 2017 HootSuite Media Inc.
+
+// This file is part of OwlBanners. The full OwlBanners copyright notice,
+// including terms governing use, modification, and redistribution, is
+// contained in the file LICENSE.md at the root of the source code distribution
+// tree.
 
 #import <UIKit/UIKit.h>
 
@@ -13,7 +12,3 @@ FOUNDATION_EXPORT double OwlBannersVersionNumber;
 
 //! Project version string for OwlBanners.
 FOUNDATION_EXPORT const unsigned char OwlBannersVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <OwlBanners/PublicHeader.h>
-
-
