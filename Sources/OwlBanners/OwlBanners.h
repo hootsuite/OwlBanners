6ca5ca2efc7bcc1356a0567317dc76f3c1624a2b
Alejandro Isaza <alejandro.isaza@hootsuite.com>
2017-11-07 10:00:29 -0800

    CMOB-501: Reorganize sources (#23)
    * CMOB-501: Reorganize sources
    
    Add demo apps to a single project to avoid confusion.
    
    * Fix comment block

---
+++ Sources/OwlBanners/OwlBanners.h
@@ -0,0 +1,14 @@
+// Copyright 2017 HootSuite Media Inc.
+
+// This file is part of OwlBanners. The full OwlBanners copyright notice,
+// including terms governing use, modification, and redistribution, is
+// contained in the file LICENSE.md at the root of the source code distribution
+// tree.
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OwlBanners.
+FOUNDATION_EXPORT double OwlBannersVersionNumber;
+
+//! Project version string for OwlBanners.
+FOUNDATION_EXPORT const unsigned char OwlBannersVersionString[];
