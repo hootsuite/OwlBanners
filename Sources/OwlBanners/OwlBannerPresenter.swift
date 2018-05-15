fce7281610486b11f4308255bb6b604ab217843a
Taylor Ledingham <taylor.ledingham@hootsuite.com>
2018-05-15 08:43:26 -0700

    CMOB-984: added public initilizer (#27)

--- Sources/OwlBanners/OwlBannerPresenter.swift
+++ Sources/OwlBanners/OwlBannerPresenter.swift
@@ -4,12 +4,16 @@ import Foundation
 
 /// Defines a banner presenter
 public protocol BannerPresenter {
-    func presentErrorBanner(title: String, style: DefaultBannerStyle)
+    func presentBanner(title: String, style: DefaultBannerStyle)
 }
 
 /// An `OwlBanner` implementation of a `BannerPresenter`
 public final class OwlBannerPresenter: BannerPresenter {
-    public func presentErrorBanner(title: String, style: DefaultBannerStyle) {
+
+    /// A initializes a `OwlBannerPresenter`
+    public init() { }
+
+    public func presentBanner(title: String, style: DefaultBannerStyle) {
         Banner(style, title: title).enqueue()
     }
 }
