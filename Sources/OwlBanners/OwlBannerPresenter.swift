d336d8911cf6b66e8d1e09e81965cd7b26a313d0
benjamin-hergert-hs <benjamin.hergert@hootsuite.com>
2020-07-03 12:00:40 -0700

    Presenter dependency on DefaultBannerStyle (#34)
    - The Presenter depended on the DefaultBannerStyle implementation
    directly instead of the BannerStyle protocol

--- Sources/OwlBanners/OwlBannerPresenter.swift
+++ Sources/OwlBanners/OwlBannerPresenter.swift
@@ -4,7 +4,7 @@ import Foundation
 
 /// Defines a banner presenter
 public protocol BannerPresenter {
-    func presentBanner(title: String, style: DefaultBannerStyle)
+    func presentBanner(title: String, style: BannerStyle)
 }
 
 /// An `OwlBanner` implementation of a `BannerPresenter`
@@ -13,7 +13,7 @@ public final class OwlBannerPresenter: BannerPresenter {
     /// A initializes a `OwlBannerPresenter`
     public init() { }
 
-    public func presentBanner(title: String, style: DefaultBannerStyle) {
+    public func presentBanner(title: String, style: BannerStyle) {
         Banner(style, title: title).enqueue()
     }
 }
